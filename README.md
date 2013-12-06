d08e5967a829dd2bec1112c94b1147e4b5ca8d76
rgsone <rgs@rgsone.com>
2013-12-06 16:52:04 +0100

    add sources and composer.json, update README

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # slim-controller-connector
 
-Slim Controller Connector is an extension to the Slim Framework to provide a simple way
-to connect controller with a Slim route.
+Slim `ControllerConnector` is an extension for Slim Framework who provides
+a simple way to connect a controller with a Slim route.
+
+### Features
+
+* The controller for a particular route is instantiated only if his route is matched
+* The registered controllers are instantiated only once (singleton)
 
 ## Install
 
@@ -12,9 +17,236 @@ With Composer.
 		"rgsone/slim-controller-connector": "dev-master"
 	}
 
+## Usage
+
+`ControllerConnector` provide two ways to connect Slim routes with controllers.
+
+### Single route way
+
+setup
+
+	<?php
+
+	$slim = new \Slim\Slim();
+    // ControllerConnector needs a Slim instance
+    $connector = new \Rgsone\Slim\ControllerConnector( $slim );
+
+basic usage
+
+    // connects '/' with 'MyController' and calls his 'myAction' method via GET method
+    $connector->connect( 'GET', '/', 'MyController:myAction' );
+
+    // another example
+    $connector->connect( 'GET', '/foo/', 'MyOtherController:myAction' );
+
+also accepts an array of middlewares to call, in additional parameters
+
+    $connector->connect(
+    	'GET',
+    	'/middleware/',
+    	'MyController:myAction',
+    	array(
+    		function() { echo 'middleware'; },
+    		function() { echo 'another middleware'; },
+    		// middlewares can also to be called like this
+    		'MyMiddleware:myAction'
+    		// or in any callable forms
+    	)
+    );
+
+`ControllerConnector::connect` method returns an `\Slim\Route` object, so it is possible to add a route name and/or
+a route conditions like Slim routing system
+
+    $connector->connect( 'GET', '/bar/', 'MyController:myAction' )
+    	->name( 'my.route.name' );
+
+    $connector->connect( 'GET', '/bar/:id', 'MyController:myAction' )
+    	->conditions( array( 'id' => '[0-9]+' ) )
+    	->name( 'my.route.with.id.name' );
+
+it also possible to bind multiples HTTP methods on a same route
+
+    // binds with GET and POST methods
+    $connector->connect( 'GET|POST', '/foo/bar', 'MyController:myAction' );
+
+    // binds with GET, POST, DELETE and PUT methods
+    $connector->connect( 'GET|POST|DELETE|PUT', '/foo/bar', 'MyController:myAction' );
+
+
+### Multiple routes for a same controller
+
+setup
+
+	<?php
+
+	$slim = new \Slim\Slim();
+    // ControllerConnector needs a Slim instance
+    $connector = new \Rgsone\Slim\ControllerConnector( $slim );
+
+basic usage,
+the only required parameter for each route is `action`, all others are optionnal by default,
+if the `method` parameter is not present, the default HTTP method is `GET`
+
+	$connector->connectRoutes(
+
+		'MyController',
+
+		array(
+			'/' => array(
+				'action' => 'myAction'
+			),
+			'/foo' => array(
+				'action' => 'myAction',
+				'method' => 'GET'
+			),
+			'/bar' => array(
+				'action' => 'myAction',
+				'method' => 'POST'
+			)
+		)
+
+	);
+
+it is possible to bind multiples HTTP methods for a same route,
+like `ControllerConnector::connect` each method must be separated by a `|`
+
+	$connector->connectRoutes(
+
+		'MyController',
+
+		array(
+			'/foo/foo' => array(
+				'action' => 'myAction',
+				// binds GET, POST and PUT methods with this route
+				'method' => 'GET|POST|PUT'
+			)
+		)
+
+	);
+
+in addition, it is possible to name route, add conditions and add a middlewares for the same route
+
+	$connector->connectRoutes(
+
+		'MyController',
+
+		array(
+			'/foo/:id' => array(
+				'action' => 'myAction',
+				'method' => 'GET|POST',
+				// add conditions for :id
+				'conditions' => array(
+					'id' => '[0-9]+'
+				),
+				// names this route
+				'name' => 'route.foo.name',
+				// middlewares accepts any callable
+				'middlewares' => array(
+					function() { echo 'route middleware'; },
+					// middlewares can also be called like this
+					'MyMiddleware:myAction'
+				)
+			)
+		)
+
+	);
+
+as a last, a globals middlewares can be defined,
+a global middleware is a middleware who is called for each declared route
+
+	$connector->connectRoutes(
+
+		'MyController',
+
+		array(
+			'/foo/bar' => array(
+				'action' => 'myAction'
+			),
+			'/bar/foo' => array(
+				'action' => 'myOtherAction'
+			)
+		),
+
+		// this middlewares will be called for '/foo/bar' and '/bar/foo' routes
+		function() { echo 'global middleware'; },
+		'MyMiddleware:myAction'
+
+	);
+
+full example
+
+	$connector->connectRoutes(
+
+		'MyController',
+
+		array(
+			'/foobar' => array(
+				'method' => 'GET',
+				'action' => 'myAction',
+				'name' => 'route.foobar',
+				'middlewares' => array(
+					'MyMiddleware:myAction'
+				)
+			),
+			'/foobar/:id' => array(
+				'method' => 'GET|POST',
+				'action' => 'myOtherAction',
+				'name' => 'route.foobar.id',
+				'conditions' => array( 'id' => '[0-9]+' ),
+				'middlewares' => array(
+					function() { echo 'single route middleware'; }
+				)
+			)
+		),
+
+		function() { echo 'global middleware'; },
+		'MyMiddleware:myAction'
+
+	);
+
+### Addition
+
+The `\Slim\Slim` instance is passed in constructor parameters of each controller.
+
+	<?php
+
+	// example of controller
+
+	class MyController
+	{
+		private $slimApp;
+
+		public function __construct( \Slim\Slim $slim )
+		{
+			$this->slimApp = $slim;
+		}
+	}
+
+## Configuration
+
+`ControllerConnector` allows to define a namespace prefix.
+If controllers are in `\\App\\Controller` namespace, configure the namespace prefix like this :
+
+	<?php
+
+	$connector = new \Rgsone\Slim\ControllerConnector( new \Slim\Slim() );
+	$connector->setNamespacePrefix( '\\App\\Controller' );
+
+And this following route
+
+	$connector->connect( 'GET', '/', 'HomeController:showIndex' );
+
+is translates to
+
+	$connector->connect( 'GET', '/', '\\App\\Controller\\HomeController:showIndex' );
+
 ## Author
 
 [rgsone aka Rudy Marc](http://rgsone.com)
+
+## Thanks
+
+to [Josh Lockhart](https://github.com/codeguy) for Slim Framework.
 
 ## License
 
---
+++ src/Rgsone/Slim/ControllerConnector.php
@@ -0,0 +1,286 @@
+<?php
+
+/* ################################################
+###################################################
+#
+#   Slim ControllerConnector is an extension
+#	for Slim Framework who provides
+#	a simple way to connect a controller
+#	with a Slim route.
+#
+#   @author	rgsone aka rudy marc
+#   @web	http://rgsone.com
+#
+###################################################
+################################################ */
+
+namespace Rgsone\Slim;
+
+use Slim\Route;
+
+/**
+ * Class ControllerConnector
+ * @package Rgsone\Slim
+ */
+class ControllerConnector
+{
+	############################################################################
+	//// PRIVATE PROP //////////////////////////////////////////////////////////
+	############################################################################
+
+	/** @var \Slim\Slim Slim instance */
+	protected $_slimApp;
+	/** @var array Controllers list */
+	protected $_controllers = array();
+	/** @var string Namespace prefix, ex : '\\Foo\\Bar' */
+	protected $_namespacePrefix;
+
+	############################################################################
+	//// CONSTRUCTOR ///////////////////////////////////////////////////////////
+	############################################################################
+
+	/**
+	 * @param \Slim\Slim $slim Slim instance
+	 */
+	public function __construct( \Slim\Slim $slim )
+	{
+		$this->_slimApp = $slim;
+		$this->_namespacePrefix = '\\';
+	}
+
+	############################################################################
+	//// PRIVATE METHOD ////////////////////////////////////////////////////////
+	############################################################################
+
+	/**
+	 * @param \Closure $callable
+	 * @return callable
+	 */
+	protected function share( \Closure $callable )
+	{
+		return function () use ( $callable ) {
+
+			static $object;
+
+			if ( null === $object )
+			{
+				$object = $callable();
+			}
+
+			return $object;
+
+		};
+	}
+
+	/**
+	 * @param string $name Controller name, ex. : 'MyController'
+	 */
+	protected function register( $name )
+	{
+		$controller = $this->_namespacePrefix . $name;
+
+		if ( !array_key_exists( $controller, $this->_controllers ) )
+		{
+			$this->_controllers[ $controller ] = $this->share( function() use ( $controller ) {
+
+				return new $controller( $this->_slimApp );
+
+			});
+		}
+	}
+
+	/**
+	 * Return the same unique instance of asked controller (singleton)
+	 * @param string $name Controller name, ex. : 'MyController'
+	 * @return object Instance of controller
+	 */
+	protected function getController( $name )
+	{
+		return $this->_controllers[ $this->_namespacePrefix . $name ]();
+	}
+
+	############################################################################
+	//// PUBLIC METHOD /////////////////////////////////////////////////////////
+	############################################################################
+
+
+	/**
+	 * Connects a route with her controller/action.
+	 *
+	 * Examples :
+	 * connect( 'GET',      '/',		 	 'Controller:action' );
+	 * connect( 'GET|POST', '/foo/bar/:id/', 'Controller:action' );
+	 * connect( 'GET',      '/foo/',		 'Controller:action', array( function() { echo 'middleware'; } ) );
+	 * connect( 'POST',     '/foo/bar/:id/', 'Controller:action', array( 'Middleware1:action', 'Middleware2:action' ) );
+	 *
+	 * @param string 	 $method 		HTTP Method,
+	 * 									ex : 'GET' match GET method
+	 * 									multiple methods are allowed, separate them by '|',
+	 * 						 			ex : 'GET|POST|UPDATE' match GET, POST, and UPDATE method
+	 *
+	 * @param string 	 $pattern 		Route pattern, like Slim route pattern,
+	 * 						  			ex : '/' or '/foo/bar/:id'
+	 *
+	 * @param string 	 $controller 	Controller and action to call,
+	 * 							 		ex. : 'MyController:myAction'
+	 *
+	 * @param null|array $middleware 	List of middlewares to call
+	 *
+	 * @return \Slim\Route
+	 */
+	public function connect( $method, $pattern, $controller, $middleware = null )
+	{
+		$httpMethod = explode( '|', $method );
+
+		$ctrlCall = explode( ':', $controller );
+		$controllerName = $ctrlCall[0];
+		$actionName = $ctrlCall[1];
+
+		$this->register( $controllerName );
+
+		$route = new Route( $pattern, function () use ( $controllerName, $actionName ) {
+
+			$args = func_get_args();
+			$ctrl = $this->getController( $controllerName );
+			return call_user_func_array( array( $ctrl, $actionName ), $args );
+
+		});
+
+		foreach ( $httpMethod as $m )
+		{
+			$route->via( $m );
+		}
+
+		if ( null !== $middleware && is_array( $middleware ) )
+		{
+			foreach ( $middleware as $mw )
+			{
+				if ( is_callable( $mw ) )
+				{
+					$route->setMiddleware( $mw );
+				}
+				elseif ( is_string( $mw ) )
+				{
+					$mwCall = explode( ':', $mw );
+
+					$this->register( $mwCall[0] );
+
+					$route->setMiddleware( function() use ( $mwCall ) {
+						call_user_func(array(
+							$this->getController( $mwCall[0] ),
+							$mwCall[1]
+						));
+					});
+				}
+			}
+		}
+
+		$this->_slimApp->router->map( $route );
+
+		return $route;
+	}
+
+	/**
+	 * Connects a controller with multiples routes.
+	 *
+	 * Usage :
+	 *
+	 * ControllerConnector::connectRoutes(
+	 * 		'MyController',
+	 * 		array(
+	 *			'/foo' => array(
+	 * 				'action' => 'myAction',
+	 * 				'param' => '',
+	 * 				'param' => '',
+	 * 				...
+	 * 			)
+	 * 		)
+	 * 		[, middleware, middleware, ...]
+	 * );
+	 *
+	 * @param string $controller Controller name, ex. : 'MyController'
+	 * @param array $routes List of routes and their parameters
+	 * @param callable Globals middlewares, called for each route
+	 */
+	public function connectRoutes( $controller, array $routes )
+	{
+		$this->register( $controller );
+
+		if ( func_num_args() > 2 )
+		{
+			$globalsMiddlewares = array_slice( func_get_args(), 2 );
+		}
+
+		foreach ( $routes as $pattern => $params )
+		{
+			if ( !isset( $params['action'] ) )
+			{
+				throw new \Exception( 'route action parameter is require' );
+			}
+
+			$route = new Route( $pattern, function() use ( $params, $controller ) {
+
+				$args = func_get_args();
+				$ctrl = $this->getController( $controller );
+				return call_user_func_array( array( $ctrl, $params['action'] ), $args );
+
+			});
+
+			$params['method'] = ( isset( $params['method'] ) && is_string( $params['method'] ) ) ? $params['method'] : 'GET';
+			$methods = explode( '|', $params['method'] );
+
+			foreach ( $methods as $m )
+			{
+				$route->via( $m );
+			}
+
+			if ( isset( $params['name'] ) && is_string( $params['name'] ) )
+			{
+				$route->name( $params['name'] );
+			}
+
+			if ( isset( $params['conditions'] ) && is_array( $params['conditions'] ) )
+			{
+				$route->conditions( $params['conditions'] );
+			}
+
+			$middlewares = ( isset( $globalsMiddlewares ) ) ? $globalsMiddlewares : array();
+
+			$middlewares = ( isset( $params['middlewares'] ) && is_array( $params['middlewares'] ) )
+						   ? array_merge( $middlewares, $params['middlewares'] )
+						   : $middlewares;
+
+			foreach ( $middlewares as $mw )
+			{
+				if ( is_callable( $mw ) )
+				{
+					$route->setMiddleware( $mw );
+				}
+				elseif ( is_string( $mw ) )
+				{
+					$call = explode( ':', $mw );
+					$this->register( $call[0] );
+
+					$route->setMiddleware( function() use ( $call ) {
+						call_user_func(array(
+							$this->getController( $call[0] ),
+							$call[1]
+						));
+					});
+				}
+
+			}
+
+			$this->_slimApp->router()->map( $route );
+		}
+	}
+
+	/**
+	 * Set controllers namespace prefix
+	 * @param string $namespacePrefix Namespace prefix, ex. : '\\Foo\\Bar'
+	 */
+	public function setNamespacePrefix( $namespacePrefix )
+	{
+		$this->_namespacePrefix = $namespacePrefix . '\\';
+	}
+} 
